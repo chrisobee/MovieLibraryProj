eb5d49deda42edd4b623c83ad26f48e099f79000
Chris O'Brien <chrisobrien2000@outlook.com>
2020-05-05 14:58:44 -0500

    Added logic to edit movie info

--- Client/app.js
+++ Client/app.js
@@ -22,6 +22,7 @@
         $("#movieInfo").html(`<li>Title: ${data["title"]}</li>`);
         $("#movieInfo").append(`<li>Director: ${data["director"]}</li>`);
         $("#movieInfo").append(`<li>Genre: ${data["genre"]}</li>`);
+        $("#editMovie").html(`<button><a href="edit.html?${movieId}">Edit Movie</a></button>`);
     });
 })(jQuery);
 
@@ -54,8 +55,21 @@
 })(jQuery);
 
 (function($){
+    let movieId = location.search.slice(1);
+
+    $.get(`https://localhost:44325/api/movie/${movieId}`, function(data){
+        $("#edit-form").html(`<input type="hidden" name="movieId" value="${movieId}" />`);
+        $("#edit-form").append(`<input type="text" name="title" value="${data["title"]}" />`);
+        $("#edit-form").append(`<input type="text" name="director" value="${data["director"]}" />`);
+        $("#edit-form").append(`<input type="text" name="genre" value="${data["genre"]}" />`);
+        $("#edit-form").append(`<button type="submit">Submit</button>`);
+    })
+})(jQuery);
+
+(function($){
     function processForm( e ){
         var dict = {
+            MovieId : parseInt(this["movieId"].value),
             Title : this["title"].value,
             Director : this["director"].value,
             Genre : this["genre"].value
